83da302aed4db709fbc94c7835556b740de55d3a
Jean Paul <jduarte@sistemasmitdev.com>
2023-07-24 16:08:40 -0500

    Correccion timezone en el servidor

--- src/helpers/getters.js
+++ src/helpers/getters.js
@@ -2,7 +2,6 @@ const fullDate = () => {
     const date = new Date();
     const options = {  timeZone: 'America/Caracas' , year: "numeric", month: "2-digit", day: "2-digit" };
     const fecha = date.toLocaleString("es-VE", options).split("/").reverse().join("");
-    console.log(fecha);
     return fecha;
 };
 
